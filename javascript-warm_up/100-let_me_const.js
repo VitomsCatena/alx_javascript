9eb785ff0676dfd32df934e69d8a6acec9aee537
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-02 10:54:57 +0300

    Create 100-let_me_const.js

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,8 @@
+// This is your initial value for myVar
+var myVar = 0;
+
+// Modify the value of myVar to 333
+myVar = 333;
+
+// You can also log the updated value to the console
+console.log("myVar is now:", myVar);
