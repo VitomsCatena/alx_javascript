553c4b0aef1fbfaf28af59c502a6904d733e9d31
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-04 10:03:08 +0300

    Update 12-object.js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
-myObject.value = 89; // Update the value property
-
+// Updating the value property
+myObject.value = 89; 
 console.log(myObject);
