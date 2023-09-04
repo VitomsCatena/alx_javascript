44bb56d96fd85f783d95e0354c3723274d564e5b
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-04 10:57:13 +0300

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-
-// Define the incr function
-function incr(number) {
-  return number + 1;
-}
-
-// Define the addMeMaybe function
-function addMeMaybe(number, theFunction) {
-  const incrementedNumber = incr(number); // Increment the number
-  theFunction(incrementedNumber); // Call the function with the incremented number
-}
-
-// Export both functions so they can be used outside of this module (CommonJS style)
-module.exports = {
-  addMeMaybe: addMeMaybe,
-  incr: incr
+const myObject = {
+  type: 'object',
+  value: 12
 };
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
