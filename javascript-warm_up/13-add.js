7d823dde44b07223301a83de92ed5d055d77c089
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-04 10:36:06 +0300

    Update 13-add.js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const { add } = require('./13-add');
+
+const add = require('./13-add').add;
 console.log(add(3, 5));
+
+// function to add two integers
+// function add(num1,num2){
+//     return num1+num2
+// }
+// console.log(add(7,9));
 //function
 function add(a, b) {
   return a + b;
