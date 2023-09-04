c77458db27444778337efe6d624a153c31d18666
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-04 10:29:09 +0300

    13-add.js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-// Example usage:
-const result = add(5, 7);
-console.log(result); // This will print 12
+const add = require('./13-add').add;
+console.log(add(3, 5));
