23bda9df33d2628a317d88a6deee7dcda5ee7dae
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 11:37:13 +0300

    Updated 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    //constructor of the parent class (Rectangle) using super()
+    super(size, size);
+  }
+}
+
+module.exports = Square;
+
+// Example usage:
+if (require.main === module) {
+  const square = new Square(4);
+  console.log(`Square Width: ${square.width}`);
+  console.log(`Square Height: ${square.height}`);
+}
