eb4b9967ad90358d7b0de50ee755e772dbcafb42
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 11:11:58 +0300

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,35 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // Return an empty object for invalid inputs
+    }
+    
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
+
+// Example usage:
+if (require.main === module) {
+  const r1 = new Rectangle(2, 3);
+  console.log(r1);
+  console.log(r1.width);
+  console.log(r1.height);
+
+  const r2 = new Rectangle(2, -3);
+  console.log(r2);
+  console.log(r2.width);
+  console.log(r2.height);
+
+  const r3 = new Rectangle(2);
+  console.log(r3);
+  console.log(r3.width);
+  console.log(r3.height);
+
+  const r4 = new Rectangle(0, 5);
+  console.log(r4);
+}
