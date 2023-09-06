55ea2b8878519d0957b3e73ca67faf0e63884be3
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 10:33:10 +0300

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -7,12 +7,12 @@ class Rectangle {
   }
 }
 
-const r1 = new Rectangle(2, 3);
+const r1 = new Rectangle(3, 3);
 console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
 
-const r2 = new Rectangle(2, -3);
+const r2 = new Rectangle(3, -3);
 console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
