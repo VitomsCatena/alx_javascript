217a29c38fd54707272836cb08968f875193a6eb
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 10:18:11 +0300

    Update 1-rectangle.js task

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h, color) {
+    this.width = w;
+    this.height = h;
+    this.color = color;
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+}
