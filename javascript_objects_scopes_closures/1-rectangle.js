1d3b6652ffcb823f97c61b6b1990e80f2ebb5bb3
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 11:06:29 +0300

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,11 +2,38 @@
 
 class Rectangle {
   constructor(w, h) {
-    this.width = w;
-    this.height = h;
+    if (w <= 0 || h <= 0) {
+      // Handle the case where width or height is not positive
+      return {};
+    } else if (h === undefined) {
+      // Handle the case where height is not provided
+      this.width = w;
+      this.height = undefined;
+    } else {
+      // Normal case, both width and height are provided
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
-// Example usage:
-const myRectangle = new Rectangle(5, 10);
-console.log(`Width: ${myRectangle.width}, Height: ${myRectangle.height}`);
+module.exports = Rectangle;
+
+// usage:
+
+if (require.main === module) {
+  const r1 = new Rectangle(2, 3);
+  console.log(r1);
+  console.log(r1.width);
+  console.log(r1.height);
+
+  const r2 = new Rectangle(2, -3);
+  console.log(r2);
+  console.log(r2.width);
+  console.log(r2.height);
+
+  const r3 = new Rectangle(2);
+  console.log(r3);
+  console.log(r3.width);
+  console.log(r3.height);
+}
