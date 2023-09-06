cb4c250d3d3245e943a3a2445827c08366baaf84
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 10:08:56 +0300

    0-rectangle.js task

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+
+  calcPerimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
