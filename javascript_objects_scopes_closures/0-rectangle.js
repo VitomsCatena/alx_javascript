d30652dc0d65f423393c368900221cd32b37299f
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-06 10:14:53 +0300

    Update 0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,19 @@
 #!/usr/bin/node
 
-const Rectangle = require('./0-rectangle');
+class Rectangle {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
 
+  calcArea() {
+    return this.width * this.height;
+  }
+
+  calcPerimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
 const r1 = new Rectangle();
 console.log(r1);
-console.log(r1.constructor);
