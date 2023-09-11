9e90b275eb3021a601e859ef75ce3db06c788ede
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-11 12:38:15 +0300

    script that display the status code of a GET request

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,1 +1,26 @@
+#!/usr/bin/env node
 
+"use strict";
+
+const request = require("request");
+
+if (process.argv.length !== 3) {
+  console.error("Usage: node script.js <URL>");
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+
+  if (response && response.statusCode) {
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    console.error("Unable to retrieve the status code.");
+    process.exit(1);
+  }
+});
