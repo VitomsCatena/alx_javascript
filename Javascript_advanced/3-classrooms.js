bd3ab01bcb877074cecc439921c93aa4ad72fbef
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 12:27:20 +0300

    Update 3-classrooms.js

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+
+function createClassRoom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    };
+  }
+
+  var students = [];
+
+  for (var i = 0; i < numbersOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  }
+
+  return students;
+}
+
+var classRoom = createClassRoom(10);
+
+ //  returns 1
+console.log(classRoom[0]());
+
+//  returns 4
+console.log(classRoom[3]());
+
+//  returns 10
+console.log(classRoom[9]());
