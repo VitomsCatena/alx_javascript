6938b3fb2d6afa50910ba5826b3cd8126757a683
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 12:30:33 +0300

    Update 7-timeout.js

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+console.log("Start of the execution queue");
+
+// Use a loop to log numbers 1 to 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+console.log("End of the loop printing");
+
+// Schedule the final code block to be executed with a delay of 0 milliseconds
+setTimeout(function () {
+  console.log("Final code block to be executed");
+}, 0);
