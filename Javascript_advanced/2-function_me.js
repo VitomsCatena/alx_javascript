b95bfb1504104f6bbd72d4cef96da1f36d04cdb2
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 12:17:32 +0300

    Update 2-function_me.js

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+function welcomeMessage(fullName) {
+  return function () {
+    alert('Welcome ' + fullName);
+  };
+}
+
+var guillaume = welcomeMessage('Guillaume');
+var alex = welcomeMessage('Alex');
+var fred = welcomeMessage('Fred');
