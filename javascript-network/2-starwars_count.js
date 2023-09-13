429aae613ef538ff86ed7590a0edeee4ba7b1a81
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 10:21:05 +0300

    Updated 2-starwars_count.js

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
 
-"use strict";
+const request = require('request');
 
-const request = require("request");
-
-if (process.argv.length !== 3) {
-  console.error("Usage: node script.js <API URL>");
+// Check if the user provided the API URL as an argument
+if (process.argv.length < 3) {
+  console.error('Usage: node starwars_count.js <api_url>');
   process.exit(1);
 }
 
+// Get the API URL from the command line argument
 const apiUrl = process.argv[2];
 
-request.get(apiUrl, (error, response, body) => {
+// Make an HTTP GET request to the API
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-
-  if (response && response.statusCode === 200) {
-    try {
-      const filmData = JSON.parse(body);
-      const wedgeAntillesMovies = filmData.results.filter((film) => {
-        return film.characters.includes("https://swapi-api.alx-tools.com/api/people/18/");
-      });
-
-      console.log(`Number of movies with Wedge Antilles: ${wedgeAntillesMovies.length}`);
-    } catch (parseError) {
-      console.error("Error parsing API response.");
-      process.exit(1);
-    }
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('HTTP Status Code:', response.statusCode);
   } else {
-    console.error("Unable to retrieve movie information.");
-    process.exit(1);
+    try {
+      // Parse the JSON response
+      const filmsData = JSON.parse(body);
+
+      // Filter movies that include "Wedge Antilles" (character ID 18)
+      const moviesWithWedgeAntilles = filmsData.results.filter((movie) =>
+        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+
+      // Print the count of movies with Wedge Antilles
+      console.log(moviesWithWedgeAntilles.length);
+    } catch (parseError) {
+      console.error('Error parsing JSON response:', parseError);
+    }
   }
 });
