5775b67b28ca99e2dd9b90ead2fe109a7f0b08a4
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 12:28:58 +0300

    Update 5-mode.js

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,1 +1,37 @@
 #!/usr/bin/env node
+function changeMode(size, weight, transform, background, color) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  };
+}
+
+function main() {
+  const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+  const welcomeParagraph = document.createElement('p');
+  welcomeParagraph.textContent = 'Welcome Holberton!';
+  document.body.appendChild(welcomeParagraph);
+
+  const spookyButton = document.createElement('button');
+  spookyButton.textContent = 'Spooky';
+  spookyButton.addEventListener('click', spooky);
+  document.body.appendChild(spookyButton);
+
+  const darkModeButton = document.createElement('button');
+  darkModeButton.textContent = 'Dark mode';
+  darkModeButton.addEventListener('click', darkMode);
+  document.body.appendChild(darkModeButton);
+
+  const screamModeButton = document.createElement('button');
+  screamModeButton.textContent = 'Scream mode';
+  screamModeButton.addEventListener('click', screamMode);
+  document.body.appendChild(screamModeButton);
+}
+
+main();
