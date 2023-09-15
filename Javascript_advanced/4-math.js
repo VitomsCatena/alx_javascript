3093b79a1ee7b0421ebb1b4ee9fad1484dc46eed
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-15 09:14:09 +0300

    4-math. task

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -1,27 +1,21 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-// Function to create a closure that divides a number by a fixed number
-function divideBy(firstNumber) {
-  return function(secondNumber) {
-    return secondNumber / firstNumber;
-  };
+function divideBy (firstNumber) {
+    function dividend (secondNumber) {
+        return secondNumber / firstNumber;
+    }
+
+    return dividend;
 }
 
-// Function to create a closure that adds a fixed number to another number
-function addBy(firstNumber) {
-  return function(secondNumber) {
-    return secondNumber + firstNumber;
-  };
+function addBy (firstNumber) {
+    function sum (secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    return sum;
 }
 
-// Create the closures as requested
 const addBy100 = addBy(100);
 const addBy1000 = addBy(1000);
 const divideBy10 = divideBy(10);
 const divideBy100 = divideBy(100);
-
-// Test cases
-console.log(addBy100(20));     // Should display 120
-console.log(divideBy10(20));   // Should display 2
-console.log(divideBy100(200)); // Should display 2
-console.log(addBy1000(20));    // Should display 1020
